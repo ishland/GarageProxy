4cad12bf446bb304b3841f725fb4c3d9d1f086c0
ishland <ishlandmc@yeah.net>
2018-04-14 19:01:04 +0800

    Update config.php

--- main/server-side/defaults/config.php
+++ main/server-side/defaults/config.php
@@ -1,8 +1,20 @@
 <?php
 //This is the configration of GarageProxy.
-//Config format: setWorker(string $lisening, string $remote-server, int $worker-count)
-//$listening and $remote-server: [Protocol]://[Address]:[Port]
-//Example: setWorker("tcp://0.0.0.0:12345", "tcp://233.233.233.233:26777", 10);
-//Warning: This version can only set one worker, or start fail.
 
-setWorker("tcp://0.0.0.0:12345", "tcp://www.google.com:80", 6);
+define('CONFIG', Array(
+    'workers' => Array(
+        Array(
+            'addr' => "tcp://0.0.0.0:12345",
+            'remote' => "tcp://www.google.com",
+            'processes' => 6
+        ),
+        Array(
+            'addr' => "tcp://0.0.0.0:12345",
+            'remote' => "tcp://www.youtube.com",
+            'processes' => 6
+        )
+    ),
+    'settings' => Array(
+        'mode' => 1
+    )
+));
