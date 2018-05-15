98879e7f3fa44dc84db272ce3cb1a9cd21e385cc
ishland <ishlandmc@yeah.net>
2018-05-15 18:14:44 +0800

    Update

--- main/server-side/functions.php
+++ main/server-side/functions.php
@@ -36,7 +36,6 @@ function checkEverything(){
         $errCount++;
     }
     $file = fopen("./.tmp", "a");
-    if(!$file) break;
     if(!fputs($file, ".")){
         echo "[  Error ] Writing failed.            \n";
         $errCount++;
